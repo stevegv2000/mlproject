281d16911c1462853d1600e124fda1ccb063aa75
stevegv2000 <stevegv2000@gmail.com>
2025-11-04 14:19:00 +0530

    Data Ingestion script

--- src/logger.py
+++ src/logger.py
@@ -4,9 +4,11 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+# os.makedirs(logs_path, exist_ok=True)
+os.makedirs(os.path.dirname(logs_path), exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+# LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+LOG_FILE_PATH = logs_path
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
@@ -14,5 +16,5 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-# if __name__ == "__main__":
-#     logging.info("Logging has started")
+if __name__ == "__main__":
+    logging.info("Logging has started")
