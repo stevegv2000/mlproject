51b8739aa0c10464f956132b7461879ac88949dd
stevegv2000 <stevegv2000@gmail.com>
2025-11-08 22:04:31 +0530

    Prediction Pipeline and Flask App commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -66,8 +66,6 @@ if __name__ == "__main__":
     model_trainer=ModelTrainer()
     print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
-
-
 # class DataIngestionConfig:
 #     def __init__(self):
 #         artifacts_dir = "artifacts"
